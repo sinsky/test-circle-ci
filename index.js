f542779bf1068e3ec098b3b8a4806b139c115ea5
sinsky <60662802+sinsky@users.noreply.github.com>
2022-01-28 03:20:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// https://www.30secondsofcode.org/js/s/hash-browser
-const hashBrowser = val =>
-  crypto.subtle
-    .digest('SHA-256', new TextEncoder('utf-8').encode(val))
-    .then(h => {
-      let hexes = [],
-        view = new DataView(h);
-      for (let i = 0; i < view.byteLength; i += 4)
-        hexes.push(('00000000' + view.getUint32(i).toString(16)).slice(-8));
-      return hexes.join('');
-    });
+// https://www.30secondsofcode.org/js/s/hash-node
+const crypto = require('crypto');
 
-hashBrowser("hello cicle").then(hash=>console.log(hash));
+const hashNode = val =>
+  new Promise(resolve =>
+    setTimeout(
+      () => resolve(crypto.createHash('sha256').update(val).digest('hex')),
+      0
+    )
+  );
+
+hashNode("hello cicle").then(hash=>console.log(hash));
