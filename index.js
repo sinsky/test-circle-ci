fdcead7c9ec25170209a2a9916b39084b584f638
sinsky <60662802+sinsky@users.noreply.github.com>
2022-01-29 04:37:29 +0900

    change index.js

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ const hashNode = val =>
 
 hashNode("Hello CircleCI!").then(hash=>console.log(hash));
 hashNode("Welcome CI/CD.").then((hash) => console.log(hash));
+hashNode('test eslint pre-commit?').then(hash => console.log(hash));
