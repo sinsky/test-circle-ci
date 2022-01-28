68c1f4c1bbf39186e17926021688a269b78a901a
sinsky <60662802+sinsky@users.noreply.github.com>
2022-01-29 05:06:52 +0900

    fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // https://www.30secondsofcode.org/js/s/hash-node
 const crypto = require("crypto");
 
-const hashNode = val =>
-  new Promise(resolve =>
+const hashNode = (val) =>
+  new Promise((resolve) =>
     setTimeout(
       () => resolve(crypto.createHash("sha256").update(val).digest("hex")),
       0
     )
   );
 
-hashNode("Hello CircleCI!").then(hash=>console.log(hash));
+hashNode("Hello CircleCI!").then((hash) => console.log(hash));
 hashNode("Welcome CI/CD.").then((hash) => console.log(hash));
-hashNode("test eslint pre-commit?").then(hash => console.log(hash));
+hashNode("test eslint pre-commit?").then((hash) => console.log(hash));
 hashNode("test eslint pre-commit?").then((hash) => console.log(hash));
